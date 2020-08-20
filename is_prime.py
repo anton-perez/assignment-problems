49a60a92628d51dfcecd064232afbc88217a5072
Anton Perez <anton.sungho.perez@gmail.com>
2020-08-20 01:30:45 +0000

    finished all problems

---
+++ is_prime.py
@@ -0,0 +1,21 @@
+import math
+
+def is_prime(n):
+  prime_boolean = True
+  for m in range(2,math.floor(n/2)):
+    if n%m == 0:
+      prime_boolean = False
+      
+
+  return prime_boolean
+
+print("testing is_prime on input 59...")
+assert is_prime(59) == True, "is_prime(59) should equal True"
+print("PASSED")
+
+print("testing is_prime on input 51...")
+assert is_prime(51) == False, "is_prime(51) should equal False"
+print("PASSED")
+
+
+
---
+++ letters_numbers_conversion.py
@@ -0,0 +1,24 @@
+letters = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def convert_to_numbers(input_string):
+  output_nums = []
+  for char in input_string:
+    output_nums.append(letters.index(char))
+
+  return output_nums
+
+def convert_to_letters(input_list):
+  output_str = ""
+  for num in input_list:
+    output_str += letters[num]
+
+  return output_str
+
+
+print("testing convert_to_numbers on input 'a cat'...")
+assert convert_to_numbers("a cat") == [1,0,3,1,20], "convert_to_numbers('a cat') should equal [1,0,3,1,20]"
+print("PASSED")
+
+print("testing convert_to_letters on input [1,0,3,1,20]...")
+assert convert_to_letters([1,0,3,1,20]) == "a cat", "convert_to_letters([1,0,3,1,20]) should equal 'a cat'"
+print("PASSED")
